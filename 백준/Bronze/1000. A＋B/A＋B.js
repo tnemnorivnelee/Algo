33c62f662b328ceed6eb6ddf140aa0bbd0488474
tnemn <63280112+tnemnorivnelee@users.noreply.github.com>
2025-01-24 00:43:51 +0900

    [Bronze V] Title: A+B, Time: 96 ms, Memory: 9344 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.js
+++ 백준/Bronze/1000. A＋B/A＋B.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 const input = fs.readFileSync('/dev/stdin').toString().trim().split(' ');
-// const input = fs.readFileSync('./input.txt').toString().trim().split('\n');
+// const input = fs.readFileSync('./input.txt').toString().trim().split(' ');
 
 console.log(Number(input[0]) + Number(input[1]));
 
