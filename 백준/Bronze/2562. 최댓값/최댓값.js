aa28dc17701acfda217f6cc1111b197d0acf9dbe
tnemn <63280112+tnemnorivnelee@users.noreply.github.com>
2025-01-28 05:11:55 +0900

    [Bronze III] Title: 최댓값, Time: 96 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(Number);
+
+console.log(Math.max(...input));
+console.log(input.indexOf(Math.max(...input)) + 1);
