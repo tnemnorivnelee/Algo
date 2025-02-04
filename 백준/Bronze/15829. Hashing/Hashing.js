a61166f798875bdeea4b7854b8d22ca0d7d079a4
tnemn <63280112+tnemnorivnelee@users.noreply.github.com>
2025-02-05 04:35:04 +0900

    [Bronze II] Title: Hashing, Time: 96 ms, Memory: 9584 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.js
+++ 백준/Bronze/15829. Hashing/Hashing.js
@@ -4,16 +4,17 @@ const input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
 const L = input[0];
 const str = input[1];
-
-const converter = (alphabet) => {
-    return `${alphabet}`.charCodeAt(0) - 96;
-}
+const M = 1234567891;
+let r = 1;
 
 let result = 0;
 
 for (let i = 0; i < L; i++) {
-    result += converter(str.charAt(i)) * (31 ** i);
+    result += (str.charCodeAt(i) - 96) * r;
+    result = result % M;
+    r = r * 31;
+    r = r % M;
 }
 
 
-console.log(result % 1234567891);
+console.log(result);
